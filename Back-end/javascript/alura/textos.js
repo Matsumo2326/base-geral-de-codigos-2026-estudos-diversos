26cbd86967980b40ab8401e0743a057a4095d37e
Matsuzim <silvaferreiramatheus10@gmail.com>
2026-08-10 11:15:43 -0300

    js codigos

---
+++ Back-end/javascript/alura/textos.js
@@ -0,0 +1,23 @@
+const cifrao = '\u0024';
+const aMaiusculo = '\u0041';
+const tique = '\u2705';
+const hiragana = '\u3041';
+
+console.log(cifrao);
+console.log(aMaiusculo);
+console.log(tique);
+console.log(hiragana);
+
+// codigo do alura
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === input); // false
+
+const senha = "minhaSenha123"
+console.log(senha.length); // 13 caracteres
+
+// JavaScript diferencia minúsculas e maiúsculas, tanto nos valores dos dados quanto no código que escrevemos.
---
+++ Back-end/javascript/alura/tipagens.js
@@ -0,0 +1,42 @@
+// tipo number +, -, *, /
+// podemos utilizar o número PI através do código Math.PI.
+const meuNumero = 3;
+const senha = "minhaSenha123"
+console.log(senha.length) // 13 caracteres
+const num1 = 1;
+const num2 = 2;
+
+const resultado = num1 + num2;
+
+console.log(resultado);
+
+
+const npflutuante = 5.5;
+const npflutuante2 = .5;
+
+const resulflutuante = npflutuante + npflutuante2
+
+console.log(resulflutuante);
+
+// not a number
+
+const numnum = 0;
+const alura = 0;
+const letranum = numnum / alura;
+console.log(letranum);
+
+
+
+// tipo string, não número nem boleano
+const texto1 = "hello world!!";
+const texto2 = 'hello world!!';
+
+const citacao = 'testando "123"'
+
+console.log(texto1)
+
+//concatenação (+)
+
+console.log(citacao + texto1)
+
+// template string ou template literal
