8b5c090a4417aecba6cf82228724e79518a25648
Matsuzim <silvaferreiramatheus10@gmail.com>
2026-08-21 07:49:33 -0300

    tipagem dinamica do js e anotações do que é o node

---
+++ Back-end/javascript/alura/06tipagemdinamica.js
@@ -0,0 +1,16 @@
+// tipagem dinamica
+// não precisa ficar tipando o tipo da var, podendo trocar a qualquer momento
+
+let minhavar = 343;
+minhavar = "Minha var";
+minhavar = true;
+
+// ES6 ou ES2015 - versão de 2015, grande up
+
+
+// linguagem interpretada - le e interpreta direto
+
+// linguagem compilada - compila e transforma em codigo de maquina
+
+// NodeJS - poder ler e interpretar o codigo JS (sem o navegador), 
+// Node é BACKEND (motivo do curso)
